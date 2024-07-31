1715e27c8e82db3b4b28639f2bff21204cb84909
anurag987655 <108646328+anurag987655@users.noreply.github.com>
2024-07-31 23:37:50 +0545

    Add files via upload

---
+++ CN assignment by anurag/assignment 8/webapp.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template_string
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+# Ensure upload folder exists
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        if file:
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+            return f'File {file.filename} uploaded successfully'
+    return '''
+    <!doctype html>
+    <title>Upload File</title>
+    <h1>Upload a File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
